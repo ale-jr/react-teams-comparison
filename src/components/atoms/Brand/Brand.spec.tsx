4a6b920b19114b7b8b2021c23a37b1bd07d8bbb5
Alexandre Fernandes Junior <alejr.dev@gmail.com>
2022-07-20 16:53:18 -0300

    test: Brand case

---
+++ src/components/atoms/Brand/Brand.spec.tsx
@@ -0,0 +1,11 @@
+import { render } from 'testUtils'
+import 'jest-styled-components'
+import Brand from './index'
+
+
+describe("Brand", () => {
+    it("Should match snapshot", () => {
+        const { asFragment } = render(<Brand />)
+        expect(asFragment()).toMatchSnapshot()
+    })
+})
