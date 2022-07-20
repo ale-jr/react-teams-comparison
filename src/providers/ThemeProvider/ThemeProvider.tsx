60412a822405e93fa8c9786c7f13da788eaa8adb
Alexandre Fernandes Junior <alejr.dev@gmail.com>
2022-07-20 11:31:10 -0300

    fix: theme provider type
    Theme provider should accept an array of elements as children

--- src/providers/ThemeProvider/ThemeProvider.tsx
+++ src/providers/ThemeProvider/ThemeProvider.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider as StyledThemeProvider } from "styled-components"
 import themes from 'themes'
 
 interface ThemeProviderProps {
-    children: JSX.Element
+    children: JSX.Element | JSX.Element[]
 }
 
 const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => <StyledThemeProvider theme={themes.default}>{children}</StyledThemeProvider>
