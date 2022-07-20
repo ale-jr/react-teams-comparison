252b70defd4699f6f698bf596e5382b91e44100b
Alexandre Fernandes Junior <alejr.dev@gmail.com>
2022-07-20 16:53:08 -0300

    chore: add react-router-dom testing support

--- src/testUtils/index.tsx
+++ src/testUtils/index.tsx
@@ -1,11 +1,12 @@
 import { render, RenderOptions, RenderResult } from '@testing-library/react'
 import ThemeProvider from 'providers/ThemeProvider'
 import { ReactElement } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 
 const Providers: React.FC<{
     children: JSX.Element | JSX.Element[]
-}> = ({ children }) => <ThemeProvider>{children}</ThemeProvider>
+}> = ({ children }) => <ThemeProvider><BrowserRouter>{children}</BrowserRouter></ThemeProvider>
 
 
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions, "queries">): RenderResult => render(ui, {
@@ -14,4 +15,4 @@ const customRender = (ui: ReactElement, options?: Omit<RenderOptions, "queries">
 })
 
 export * from '@testing-library/react'
-export {customRender as render}
+export { customRender as render }
