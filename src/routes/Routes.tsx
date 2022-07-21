1baafc09ffacb8b87fa67b99c715fc9ba7d2acd9
Alexandre Fernandes Junior <alejr.dev@gmail.com>
2022-07-21 19:41:37 -0300

    feat: implement PageWithNavigation template

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes as ReactRouterRoutes, Route } from "react-router-dom";
 import Home from "components/pages/Home"
 import NotFound from "components/pages/NotFound"
+import PageWithNavigation from "components/templates/PageWithNavigation";
 
 const Routes: React.FC = () =>
     <BrowserRouter>
-        <ReactRouterRoutes>
-            <Route path="/" element={<Home />} />
-            <Route path="*" element={<NotFound />} />
-        </ReactRouterRoutes>
+        <PageWithNavigation>
+            <ReactRouterRoutes>
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<NotFound />} />
+            </ReactRouterRoutes>
+        </PageWithNavigation>
+
     </BrowserRouter>
 export default Routes
