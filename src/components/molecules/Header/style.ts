ae2e2949eac0898a68c40c1aee2c355f5b3fa838
Alexandre Fernandes Junior <alejr.dev@gmail.com>
2022-07-21 19:36:31 -0300

    feat(Header): bring to front

--- src/components/molecules/Header/style.ts
+++ src/components/molecules/Header/style.ts
@@ -3,7 +3,8 @@ import styled from 'styled-components'
 
 export const HeaderContainer = styled.header`
     //Position and Layout
-
+    position: relative;
+    z-index: 99;
 
     //Display and visibility
     display: flex;
