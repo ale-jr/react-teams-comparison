a8267bf16441ed5c40c6b7edc75eb82f3409176c
Alexandre Fernandes Junior <alejr.dev@gmail.com>
2022-07-21 19:33:21 -0300

    fix(IconButton): height issues

--- src/components/atoms/IconButton/style.tsx
+++ src/components/atoms/IconButton/style.tsx
@@ -28,6 +28,7 @@ const IconButton = styled.button.attrs<IconButtonProps>((props) => ({
     transition: all ${({ theme }) => theme.animation.shorthand};
 
     //Box model (From outside in)
+    height: unset;
     border: none;
     border-radius: 50%;
     padding: 2px;
@@ -39,6 +40,7 @@ const IconButton = styled.button.attrs<IconButtonProps>((props) => ({
     //Typography
     color: ${({ color, colorVariant = 'default', theme }) => color ? theme.colors[color][colorVariant] : 'inherit'};
     font-size: ${({ size, theme }) => size ? theme.typography.size[size] : 'inherit'};
+    line-height: 0%;
 
     //Pseudo-classes and pseudo-elements
     &:hover{
