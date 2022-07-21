0d80f20fb58c304178947dfe16dda416bb0b6bd0
Alexandre Fernandes Junior <alejr.dev@gmail.com>
2022-07-21 19:39:54 -0300

    feat: create PageWithNavigation template

---
+++ src/components/templates/PageWithNavigation/styles.ts
@@ -0,0 +1,88 @@
+import styled from 'styled-components'
+
+
+
+export const PageWithNavigationContainer = styled.div`
+    //Position and Layout
+
+
+    //Display and visibility
+    display: flex;
+    flex-direction:column;
+    height: 100vh;
+
+    //Clipping
+
+
+    //Animation
+
+    
+    //Box model (From outside in)
+
+
+    //Background
+
+
+    //Typography
+
+
+    //Pseudo-classes and pseudo-elements
+
+`
+
+export const Content = styled.div`
+        //Position and Layout
+
+
+    //Display and visibility
+    display: flex;
+    flex-direction:row;
+    flex-grow: 1;
+
+    //Clipping
+
+
+    //Animation
+
+    
+    //Box model (From outside in)
+    min-height: 0px;
+
+    //Background
+
+
+    //Typography
+
+
+    //Pseudo-classes and pseudo-elements
+`
+
+export const Main = styled.main`
+
+    //Position and Layout
+
+
+    //Display and visibility
+    flex-grow: 1;
+    
+    
+
+    //Clipping
+    overflow-y: auto;
+
+    //Animation
+
+    
+    //Box model (From outside in)
+    min-height: 0px;
+    padding: ${({ theme }) => `${theme.spacing.lg} ${theme.spacing.xl}`};
+
+    //Background
+
+
+    //Typography
+
+
+    //Pseudo-classes and pseudo-elements
+    
+`
