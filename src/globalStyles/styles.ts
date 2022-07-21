62b5eb912523adb8200d2d50334266a29904eab6
Alexandre Fernandes Junior <alejr.dev@gmail.com>
2022-07-21 19:40:23 -0300

    feat(globalStyles): add custom scroll

--- src/globalStyles/styles.ts
+++ src/globalStyles/styles.ts
@@ -5,7 +5,30 @@ const GlobalStyle = createGlobalStyle`
     font-family: ${({ theme }) => theme.typography.font.body};
     font-size: ${({ theme }) => theme.typography.size.md};
     font-weight: ${({ theme }) => theme.typography.weight.regular};
+    
+    background-color: ${({ theme }) => theme.colors.neutral['50']};
  }
+
+ /* Works on Firefox */
+* {
+  scrollbar-width: thin;
+  scrollbar-color: blue transparent;
+}
+
+/* Works on Chrome, Edge, and Safari */
+*::-webkit-scrollbar {
+  width: 5px;
+}
+
+*::-webkit-scrollbar-track {
+  background: transparent;
+}
+
+*::-webkit-scrollbar-thumb {
+  background-color: ${({ theme }) => theme.colors.primary.default};
+  border-radius: 20px;
+  border: 3px solid transparent;
+}
 `
 
 export default GlobalStyle
