4cc63ef138a9f1408a16e823359ce6cf956337a8
Alexandre Fernandes Junior <alejr.dev@gmail.com>
2022-07-22 11:59:37 -0300

    test: ComparisonTableHeader case

---
+++ src/components/molecules/ComparisonTableHeader/ComparisonTableHeader.spec.tsx
@@ -0,0 +1,54 @@
+import 'jest-styled-components'
+import { fireEvent, render } from "testUtils"
+import ComparisonTableHeader from './index'
+
+describe("ComparisonTableHeader", () => {
+
+    it("Should match snapshot", () => {
+        const ascendingOrder = render(<table>
+            <thead>
+                <ComparisonTableHeader sort='ascending' onSelectSort={() => { }} />
+            </thead>
+        </table>)
+        expect(ascendingOrder.asFragment()).toMatchSnapshot()
+
+        const descendingOrder = render(<table>
+            <thead>
+                <ComparisonTableHeader sort='descending' onSelectSort={() => { }} />
+            </thead>
+        </table>)
+        expect(descendingOrder.asFragment()).toMatchSnapshot()
+    })
+
+    it("Should change sorting (from descending to ascending)", () => {
+        const handleSelectSort = jest.fn()
+
+        const { getByLabelText } = render(<table>
+            <thead>
+                <ComparisonTableHeader sort='descending' onSelectSort={handleSelectSort} />
+            </thead>
+        </table>)
+
+        const sortButton = getByLabelText("Sorting by descending teams, invert sorting order")
+        expect(sortButton).toBeDefined()
+        fireEvent.click(sortButton)
+        expect(handleSelectSort).toBeCalledTimes(1)
+        expect(handleSelectSort).toBeCalledWith('ascending')
+    })
+
+    it("Should change sorting (from ascending to descending)", () => {
+        const handleSelectSort = jest.fn()
+
+        const { getByLabelText } = render(<table>
+            <thead>
+                <ComparisonTableHeader sort='ascending' onSelectSort={handleSelectSort} />
+            </thead>
+        </table>)
+
+        const sortButton = getByLabelText("Sorting by ascending teams, invert sorting order")
+        expect(sortButton).toBeDefined()
+        fireEvent.click(sortButton)
+        expect(handleSelectSort).toBeCalledTimes(1)
+        expect(handleSelectSort).toBeCalledWith('descending')
+    })
+})
