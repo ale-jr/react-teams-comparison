31e05d7e170124bcbd4a32533ae8e44d40494021
Alexandre Fernandes Junior <alejr.dev@gmail.com>
2022-07-23 16:42:42 -0300

    feat(Routes): add comparison page to router

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,15 +2,19 @@ import { BrowserRouter, Routes as ReactRouterRoutes, Route } from "react-router-
 import Home from "components/pages/Home"
 import NotFound from "components/pages/NotFound"
 import PageWithNavigation from "components/templates/PageWithNavigation";
+import ComparisonTable from "components/pages/ComparisonTable";
 
-const Routes: React.FC = () =>
-    <BrowserRouter>
+
+const Routes: React.FC = () => {
+    return <BrowserRouter>
         <PageWithNavigation>
             <ReactRouterRoutes>
                 <Route path="/" element={<Home />} />
+                <Route path="/comparison" element={<ComparisonTable />} />
                 <Route path="*" element={<NotFound />} />
             </ReactRouterRoutes>
         </PageWithNavigation>
 
     </BrowserRouter>
+}
 export default Routes
