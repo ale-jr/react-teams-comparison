dbb862d2cfc85747ec7d1b592de70a52f895ad94
Alexandre Fernandes Junior <alejr.dev@gmail.com>
2022-07-25 18:05:20 -0300

    test: Button case

---
+++ src/components/atoms/Button/Button.spec.tsx
@@ -0,0 +1,13 @@
+import 'jest-styled-components'
+import Button from "./index";
+import { render } from "testUtils"
+import { faBug } from "@fortawesome/free-solid-svg-icons"
+
+describe("Button", () => {
+    it("should render icon", () => {
+        const { getByRole } = render(<Button startIcon={faBug} >Click Me!</Button>)
+        expect(getByRole("img", {
+            hidden: true
+        })).toBeDefined()
+    })
+})
