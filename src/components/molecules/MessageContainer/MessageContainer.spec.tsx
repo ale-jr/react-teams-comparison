0246b4379e5a4b149eb62fd67b9ba0b5c31a735f
Alexandre Fernandes Junior <alejr.dev@gmail.com>
2022-07-25 18:06:19 -0300

    test: MessageContainer spec

---
+++ src/components/molecules/MessageContainer/MessageContainer.spec.tsx
@@ -0,0 +1,16 @@
+import { Team } from 'interfaces/team'
+import 'jest-styled-components'
+import { render } from "testUtils"
+import MessageContainer from './index'
+
+
+describe("MessageContainer", () => {
+
+    it("should match snapshot", () => {
+        const errorComponent = render(<MessageContainer message='Oh no!' type="error" />)
+        expect(errorComponent.asFragment()).toMatchSnapshot()
+
+        const loadingContainer = render(<MessageContainer message="Fiding the best job ever!" type="loading" />)
+        expect(loadingContainer.asFragment()).toMatchSnapshot()
+    })
+})
